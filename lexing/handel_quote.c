3a0c1a54bbd1a61274c35bdefa28c52ccd4f3f0a
Ilyass Rabhi <irabhi@e3r3p12.1337.ma>
2025-01-17 13:02:39 +0100

    t5rbi9

--- lexing/handel_quote.c
+++ lexing/handel_quote.c
@@ -12,3 +12,65 @@
 
 #include "../minishell.h"
 
+int	add_char(char **str, char c)
+{
+	char	*tmp;
+	char	*tmp2;
+
+	if (*str == NULL)
+	{
+		*str = malloc(sizeof(char) * 2);
+		if (!(*str))
+			return (1);
+		*str[0] = c;
+		*str[1] = 0;
+		return (0); 
+	}
+	tmp = malloc(2);
+	if (!tmp)
+		return (1);
+	tmp[0] = c;
+	tmp[1] = '\0';
+	tmp2 = ft_strjoin(*str, tmp);
+	if (!tmp2)
+		return (free(tmp), 1);
+	free(*str);
+	free(tmp);
+	*str = tmp2;
+	return (0);
+}
+
+int	expand_var(char **str, char *line, int *itt, t_env *env)
+{
+	char	*exp_str;
+	char	*tmp;
+	int		i;
+
+	i = *itt + 1;
+	while (line[i] && !is_spaces(line[i]) && line[i] != '"' && line[i] != '\'' && line[i] != '$')
+		i++;
+	tmp = ft_substr(line, itt + 1, i);
+}
+
+int	handel_quote(t_token **token, char *line, int *i, int *error, t_env *env)
+{
+	int		close_q;
+	char	quote;
+	int		itt;
+	char	*str;
+
+	close_q = 1;
+	quote = line[*i];
+	itt = *i + 1;
+	str = NULL;
+	while (line[itt] && line[itt] != quote)
+	{
+		if (line[itt] == '$' && quote == '"')
+			expand_var(&str, line, &itt, env);
+		else
+		{
+			if(add_char(&str, line[itt]))
+				*error = 1;
+		}	
+	}
+}
--- lexing/lex.c
+++ lexing/lex.c
@@ -12,18 +12,22 @@
 
 #include "../minishell.h"
 
-int		lex(t_token **token, char *line)
+int		lex(t_token **token, char *line, t_env *env)
 {
 	int		i;
+	int		error;
 
 	i = 0;
-	while (*(line + i))
+	error = 0;
+	while (line[i])
 	{
 		while (is_spaces(line[i]))
 			i++;
 		if (is_special(line[i]))
-			handel_special(token, line, &i);
+			handel_special(token, line, &i, &error);
 		else if (is_quote(line[i]))
-			handel_quote(token, line, &i);
+			handel_quote(token, line, &i, &error, env);
+		if (error)
+			return (1);
 	}	
 }
